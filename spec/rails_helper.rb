781415edcbf276cce09ff6d0728406e67c6752a3
Artem <yegorov0725@yandex.ru>
2018-08-29 12:42:03 +0300

    End 5 chapter

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -55,3 +55,7 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+
+RSpec.configure do |c|
+  c.include Devise::Test::ControllerHelpers, type: :controller
+end
