7920a9f08d5d09b70c45379a81af040e77ae521d
Artem <yegorov0725@yandex.ru>
2018-08-31 15:17:37 +0300

    End 9 chapter

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
+  let(:subject) { FactoryBot.create(:project) }
+  it { is_expected.to validate_uniqueness_of(:name).scoped_to(:user_id) }
+
   it "does not allow duplicate project names per user" do
     user = User.create(
       first_name: "Joe",
